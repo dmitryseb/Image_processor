876b9d0ab916564a0508e1f22a767ff62ba9e5c3
Dmitry Sergienko <87063391+dmitryseb@users.noreply.github.com>
2024-03-21 09:52:50 +0300

    Delete image_processor directory

--- image_processor/image_processor.cpp
+++
@@ -1,9 +0,0 @@
-#include "pipeline.h"
-
-int main(int argc, char** argv) {
-    Pipeline pipeline;
-    pipeline.CheckReference(argc);
-    pipeline.Parse(argc, argv);
-    pipeline.Run();
-    return 0;
-}
